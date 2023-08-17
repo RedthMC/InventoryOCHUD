f22893a8f98f81930a43e64b3904062b49a0488b
ImToggle <98242902+ImToggle@users.noreply.github.com>
2023-08-18 02:55:31 +0800

    rename to HandHUD

---
+++ src/main/java/me/redth/armorochud/hud/HandHUD.java
@@ -0,0 +1,17 @@
+package me.redth.armorochud.hud;
+
+import net.minecraft.init.Items;
+import net.minecraft.item.ItemStack;
+
+public class HandHUD extends SingleItemHud {
+    private static final ItemStack exampleItem = new ItemStack(Items.diamond_sword);
+
+    public HandHUD() {
+        super(0, 492);
+    }
+
+    @Override
+    protected ItemStack getItem(boolean example) {
+        return example ? exampleItem : mc.thePlayer.getEquipmentInSlot(0);
+    }
+}
